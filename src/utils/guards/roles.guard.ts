35762afc85fd9135aa0928ada676222a75176885
dimodimow <ivanov.dimo0029@gmail.com>
2024-01-20 18:04:33 +0200

    feat: create RolesGuard

---
+++ src/utils/guards/roles.guard.ts
@@ -0,0 +1,30 @@
+import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { ROLES_KEY } from '../decorators/roles.decorator';
+import { Role } from '../enums/role.enum';
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const requiredRoles = this.reflector.getAllAndOverride<Role[]>(ROLES_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+
+    if (!requiredRoles) {
+      return true;
+    }
+
+    const req = context.switchToHttp().getRequest();
+
+    const { roles }: { roles: string[] } = req.user;
+
+    if (roles?.length === 0) {
+      return false;
+    }
+
+    return requiredRoles.some((role) => roles.includes(role));
+  }
+}
