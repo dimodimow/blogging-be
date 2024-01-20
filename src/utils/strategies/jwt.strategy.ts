6194ffd575bdf61326dcaf76ac2cefb3e2d2aa06
dimodimow <ivanov.dimo0029@gmail.com>
2024-01-20 18:05:02 +0200

    refactor: refactor the jwt strategy to return the roles

--- src/utils/strategies/jwt.strategy.ts
+++ src/utils/strategies/jwt.strategy.ts
@@ -13,6 +13,6 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   }
 
   async validate(payload: any) {
-    return { userId: payload.userId, username: payload.username };
+    return { roles: payload.roles, userId: payload.userId };
   }
 }
